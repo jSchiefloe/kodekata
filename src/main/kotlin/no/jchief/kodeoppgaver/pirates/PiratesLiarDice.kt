3649fcc08653ea5f77fdd663fc4142478964c086
Jens.Schiefloe <jens.schiefloe@nav.no>
2026-03-06 14:57:56 +0100

    bugfix! Will lyver fortsatt, men finner ikke lenger på ugyldige verdier som 37

--- src/main/kotlin/no/jchief/kodeoppgaver/pirates/PiratesLiarDice.kt
+++ src/main/kotlin/no/jchief/kodeoppgaver/pirates/PiratesLiarDice.kt
@@ -1,6 +1,7 @@
 package no.jchief.kodeoppgaver.pirates
 
-import no.jchief.kodeoppgaver.pirates.Spiller.*
+import no.jchief.kodeoppgaver.pirates.Spiller.Jens
+import no.jchief.kodeoppgaver.pirates.Spiller.Will
 import java.util.*
 
 class PiratesLiarDice(
@@ -72,7 +73,8 @@ sealed interface Spiller {
             else if (terningVerdi > eksisterendeSvar)
                 Svar.Raise(verdi = terningVerdi)
             else
-                Svar.Raise(verdi = (eksisterendeSvar + 1..66).random(), erLøgn = true)
+//                Svar.Raise(verdi = (eksisterendeSvar + 1..66).random(), erLøgn = true)
+                Svar.Raise(verdi = lagLøgnOver(eksisterendeSvar + 1), erLøgn = true)
         }
     }
 }
@@ -92,16 +94,41 @@ data class Terning(val verdi: Int)
 fun main() {
     PiratesLiarDice(
 //        terninger = Terninger.RemoteTerninger("https://piratesliarsdice.ekstern.dev.nav.no/dice").get()
-        terninger = Terninger.LokaleTerninger(0).get()
+//        terninger = Terninger.RemoteTerninger("https://piratesliarsdice.ekstern.dev.nav.no/youwin").get()
+        terninger = Terninger.RemoteTerninger("https://piratesliarsdice.ekstern.dev.nav.no/youlose").get()
+//        terninger = Terninger.RemoteTerninger("https://piratesliarsdice.ekstern.dev.nav.no/onlysixes").get()
+//        terninger = Terninger.LokaleTerninger(0).get()
 //        terninger = Terninger.LokaleTerninger(1).get()
 //        terninger = Terninger.LokaleTerninger(2).get()
+//        terninger = Terninger.LokaleTerninger(3).get()
     ).play()
 }
 
+
 fun Stack<Terning>.taTo(): Pair<Terning, Terning> = Pair(this.pop(), this.pop())
+
 
 fun Pair<Terning, Terning>.høyesteVerdi(): Int =
     if (this.first.verdi > this.second.verdi)
         "${this.first.verdi}${this.second.verdi}".toInt()
     else
         "${this.second.verdi}${this.first.verdi}".toInt()
+
+
+fun lagLøgnOver(minsteGyldigeBud: Int): Int {
+    val siffer = minsteGyldigeBud.let {
+        val tier = it / 10
+        val ener = it % 10
+        Pair(tier, ener)
+    }
+    val tilfeldigTierplass = (siffer.first..6).random()
+
+    val tilfeldigEnerplass = if (tilfeldigTierplass == siffer.first)
+            (siffer.second..6).random()
+    else (1..6).random()
+
+    return Pair(
+        Terning(tilfeldigTierplass),
+        Terning(tilfeldigEnerplass)
+    ).høyesteVerdi()
+}
